74214cf1cfa1bac793a21c4be4ba4ce39cc1c353
Nigel Ang Ru Xian <nigelangruxian@gmail.com>
2025-10-08 14:19:18 +0800

    fix issue #3304

--- db/Schedule.go
+++ db/Schedule.go
@@ -11,8 +11,8 @@ type Schedule struct {
 	LastCommitHash *string `db:"last_commit_hash" json:"-" backup:"-"`
 	RepositoryID   *int    `db:"repository_id" json:"repository_id" backup:"-"`
 
-	TaskParamsID *int       `db:"task_params_id" json:"-" backup:"-"`
-	TaskParams   TaskParams `db:"-" json:"task_params,omitempty" backup:"task_params"`
+	TaskParamsID *int        `db:"task_params_id" json:"-" backup:"-"`
+	TaskParams   *TaskParams `db:"-" json:"task_params,omitempty" backup:"task_params"`
 }
 
 type ScheduleWithTpl struct {
--- services/project/backup.go
+++ services/project/backup.go
@@ -234,7 +234,7 @@ func (b *BackupDB) format() (*BackupFormat, error) {
 			*tplName,
 		}
 
-		if o.TaskParams.InventoryID != nil {
+		if o.TaskParams != nil && o.TaskParams.InventoryID != nil {
 			schedules[i].TaskParams.InventoryName, _ = findNameByID[db.Inventory](*o.TaskParams.InventoryID, b.inventories)
 		}
 	}
